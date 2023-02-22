9a4a93b737a95734dc3957c91c63935f43919753
GitSnowflake <125999802+GitSnowflake@users.noreply.github.com>
2023-02-22 12:14:11 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,16 +19,14 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[fruits_selected];
 streamlit.dataframe(fruits_to_show);
 
-
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi');
-streamlit.write('The user entered ', fruit_choice)
-
 streamlit.header('FruityVice Fruit Advice:');
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi');
+streamlit.write('The user entered ', fruit_choice);
+
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice);
 #streamlit.text(fruityvice_response.json());
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json());
 streamlit.dataframe(fruityvice_normalized);
-
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"]);
 my_cur = my_cnx.cursor();
@@ -36,3 +34,6 @@ my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST");
 my_data_row = my_cur.fetchall();
 streamlit.header("The Fruit Load List Contains:");
 streamlit.dataframe(my_data_row);
+
+fruit_added = streamlit.text_input('What fruit would you like to add?','');
+streamlit.write(fruit_added, ' added succeffully!');
