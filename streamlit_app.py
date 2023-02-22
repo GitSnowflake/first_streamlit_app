4aa9a8013234867c04a200fe8ac4b3b2047cc900
GitSnowflake <125999802+GitSnowflake@users.noreply.github.com>
2023-02-22 14:07:08 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,16 +35,17 @@ try:
     streamlit.dataframe(get_fruityvice_data(fruit_choice));
 except URLError as e:
   streamlit.error();
-  
-streamlit.stop();
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"]);
-my_cur = my_cnx.cursor();
-my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST");
-my_data_row = my_cur.fetchall();
 streamlit.header("The Fruit Load List Contains:");
-streamlit.dataframe(my_data_row);
-
+def get_fruit_load_list():
+    with my_cnx.cursor() as my_cur:
+        my_cur.execute('SELECT * FROM FRUITE_LOAD_LIST')
+        return my_cur.fetchall();
+if streamlit.button('Get Fruit Load List'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"]);
+    my_data_row = get_fruit_load_list();
+    streamlit.dataframe(my_data_row);
+streamlit.stop();
 fruit_added = streamlit.text_input('What fruit would you like to add?','');
 my_cur.execute("INSERT INTO FRUIT_LOAD_LIST() VALUES ()");
 streamlit.write(fruit_added, ' added succeffully!');
