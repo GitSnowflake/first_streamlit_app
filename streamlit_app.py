248363cc4d0bcd5cac8c7c18d52b2a9c3e6dd731
GitSnowflake <125999802+GitSnowflake@users.noreply.github.com>
2023-02-22 15:58:19 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,7 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 my_fruit_list = my_fruit_list.set_index('Fruit');
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado','Strawberries']);
 for fruit in fruits_selected:
-    streamlit.text(fruits_selected);
+    streamlit.text(fruit);
 fruits_to_show = my_fruit_list.loc[fruits_selected];
 streamlit.dataframe(fruits_to_show);
 
