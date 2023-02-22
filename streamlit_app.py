4891ba6107c6a6dbf15df55bfd541bfa4f261cb1
GitSnowflake <125999802+GitSnowflake@users.noreply.github.com>
2023-02-22 13:47:22 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,13 +21,18 @@ fruits_to_show = my_fruit_list.loc[fruits_selected];
 streamlit.dataframe(fruits_to_show);
 
 streamlit.header('FruityVice Fruit Advice:');
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi');
-streamlit.write('The user entered ', fruit_choice);
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice);
-#streamlit.text(fruityvice_response.json());
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json());
-streamlit.dataframe(fruityvice_normalized);
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?');
+  if not fruit_choice:
+     streamlit.error('Please select a fruit to get informtaion.');
+  else:    
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice);
+    #streamlit.text(fruityvice_response.json());
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json());
+    streamlit.dataframe(fruityvice_normalized);
+except URLError as e:
+  streamlit.error();
+  
 streamlit.stop();
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"]);
@@ -38,5 +43,5 @@ streamlit.header("The Fruit Load List Contains:");
 streamlit.dataframe(my_data_row);
 
 fruit_added = streamlit.text_input('What fruit would you like to add?','');
-my_cur.execute("INSERT INTO FRUIT_LOAD_LIST() VALUES 
+my_cur.execute("INSERT INTO FRUIT_LOAD_LIST() VALUES ()");
 streamlit.write(fruit_added, ' added succeffully!');
