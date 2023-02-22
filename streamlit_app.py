9b755464dfd29a7cbab0713d2fdc0ed87447e413
GitSnowflake <125999802+GitSnowflake@users.noreply.github.com>
2023-02-22 15:38:31 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ streamlit.title("My Parents New Healthy Dinner");
 
 fruityvice_response1 = requests.get("https://fruityvice.com/api/fruit/Apple");
 fruityvice_response2 = requests.get("https://fruityvice.com/api/fruit/Orange");
-full_response = '{' + fruityvice_response1.json() + ',' + fruityvice_response2.json() +'}';
+full_response = '{'.join([fruityvice_response1.json(), ',' , fruityvice_response2.json() ,'}']);
 streamlit.text(full_response);
 fruityvice_normalized = pandas.json_normalize(full_response);
 streamlit.text(fruityvice_normalized);
