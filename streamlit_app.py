716c8decbe1765801adb01dcb5e91af2a2e88ec4
GitSnowflake <125999802+GitSnowflake@users.noreply.github.com>
2023-02-22 12:08:47 -0500

    Update streamlit_app.py
    Loading all fruits

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,15 +23,16 @@ streamlit.dataframe(fruits_to_show);
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi');
 streamlit.write('The user entered ', fruit_choice)
 
+streamlit.header('FruityVice Fruit Advice:');
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice);
 #streamlit.text(fruityvice_response.json());
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json());
 streamlit.dataframe(fruityvice_normalized);
 
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
-my_data_row = my_cur.fetchone()
-streamlit.text("The Fruit Load List Contains:")
-streamlit.text(my_data_row)
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"]);
+my_cur = my_cnx.cursor();
+my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST");
+my_data_row = my_cur.fetchall();
+streamlit.header("The Fruit Load List Contains:");
+streamlit.dataframe(my_data_row);
